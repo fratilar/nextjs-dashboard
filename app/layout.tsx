af86a3f19dcd893ff21712ce2bea637ab8d5a650
Razvan Fratila <razvan.fratila@ropardo.ro>
2024-03-04 11:21:20 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({children} : {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased hover:subpixel-antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
